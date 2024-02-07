04d3e34c89c10ca6b871840fe7eee4f4e3e5d342
duardoliveiras <duardooliveiras@gmail.com>
2024-02-07 11:25:18 +0000

    docker-compose

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,12 @@
-console.log('hello world!')
+import fastify from "fastify";
+
+
+const app = fastify();
+
+app.get('/hello', () => {
+    return 'Salve!';
+})
+
+app.listen({port: 3000}).then(() => {
+    console.log("Server is running on port 3000");
+});

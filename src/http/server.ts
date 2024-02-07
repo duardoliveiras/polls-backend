3b05d16609b04a2f6e6a80ce0b5cd87534d351ea
duardoliveiras <duardooliveiras@gmail.com>
2024-02-07 12:21:11 +0000

    controller post

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,35 @@
 import fastify from "fastify";
+import { z } from 'zod';
+import { PrismaClient } from "@prisma/client";
 
 
 const app = fastify();
 
+const prisma = new PrismaClient(); // instantiate PrismaClient
+
 app.get('/hello', () => {
     return 'Salve!';
+});
+
+app.post('/polls', async (request, reply) => {
+
+    const pollBody = z.object({ // define the object schema
+        title : z.string(),
+    });
+
+    const {title} = pollBody.parse(request.body); // throws if invalid
+    console.log(title);
+
+    const result = await prisma.poll.create({
+        data:{
+            title: title,
+        }
+    })
+
+
+        return reply.status(201).send(result);
+        // 201 is the status code for "created"
+    
 })
 
 app.listen({port: 3000}).then(() => {

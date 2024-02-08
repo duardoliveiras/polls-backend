d59fa7296ffb4718e7fe56795a322a1205a38d31
duardoliveiras <duardooliveiras@gmail.com>
2024-02-08 17:12:27 +0000

    create vote model

--- src/http/routes/vote-poll.ts
+++ src/http/routes/vote-poll.ts
@@ -4,20 +4,33 @@ import { prisma } from '../lib/prisma';
 import { randomUUID } from "node:crypto";
 
 export async function votePoll(app: FastifyInstance){
-    app.post('poll/:id/vote', async(request, reply) => {
+    app.post('/poll/:id/vote', async(request, reply) => {
         const pollOption = z.object({
             pollOptionId: z.string().uuid(),
         });
 
         const pollParam = z.object({
-            pollId: z.string().uuid(),
+            id: z.string().uuid(),
         });
 
-        const { pollOptionId } = pollOption.parse(request.params);
-        const { pollId } = pollParam.parse(request.params);
+        const { id } = pollParam.parse(request.params); // request.params is the parameters of the request
+        const { pollOptionId } = pollOption.parse(request.body); // request.body is the body of the request
+        
 
-        const sessionId = randomUUID(); // generate a random session id for the user
-        return reply.status(201).send();
+        let sessionId = request.cookies.sessionId; 
+        
+        if(!sessionId){
+        sessionId = randomUUID(); // generate a random session id for the user
+
+            reply.setCookie('sessionId', sessionId, {
+                path: '/', // set the cookie to be available on all routes
+                maxAge: 60 * 60 * 24 * 7, // set the cookie to expire in 7 days
+                signed: true, // sign the cookie to avoid the user to modify it 
+                httpOnly: true, // set the cookie to be only accessible by the server
+            });
+        }
+
+        return reply.status(201).send({ sessionId });
 
     }) 
 }
